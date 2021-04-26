e92cd1e4a2106c1bb5c4125a00abf1d8616ec087
WilliamC-08 <71800795+WilliamC-08@users.noreply.github.com>
2021-04-26 21:28:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 
-echo "hello kakak ahyar ganteng kan ?? ";
+echo "Halo, saya William Chandra, saya dari jurusan Sistem Informasi 2018, nim saya yaitu 51018021! <br>";
+echo "Terimakasih sudah berkunjung!!!"
